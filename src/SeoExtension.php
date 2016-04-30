f94ec0f8c80bc8c68a7304df60dc570ab24f94e0
Bob den Otter <bob@twokings.nl>
2016-04-30 16:12:44 +0200

    Needs more easter egg.

--- src/SeoExtension.php
+++ src/SeoExtension.php
@@ -4,6 +4,7 @@ namespace Bolt\Extension\BobdenOtter\Seo;
 
 use Bolt\Asset\File\JavaScript;
 use Bolt\Asset\File\Stylesheet;
+use Bolt\Asset\Widget\Widget;
 use Bolt\Controller\Zone;
 use Bolt\Extension\SimpleExtension;
 //use Bolt\Translation\Translator as Trans;
@@ -15,24 +16,12 @@ class SeoExtension extends SimpleExtension
 {
     private $version = "v0.10.0";
 
+    private $eastereggchance = 0.15;
+
     public function registerFields()
     {
         return [
             new SeoField(),
-        ];
-    }
-
-    public function registerServices(Application $app)
-    {
-        $seo = new SEO($this->getContainer(), $this->getConfig(), $this->version);
-        $app['twig']->addGlobal('seo', $seo);
-        $app['twig']->addGlobal('seoconfig', $this->getConfig());
-    }
-
-    protected function registerTwigPaths()
-    {
-        return [
-            'templates' => ['position' => 'prepend', 'namespace' => 'bolt']
         ];
     }
 
@@ -47,11 +36,61 @@ class SeoExtension extends SimpleExtension
         $backboneJs = new JavaScript();
         $backboneJs->setFilename('backbone-min.js')->setZone(Zone::BACKEND)->setPriority(15);
 
-        return [
+        $assets = [
             $seoCss,
             $backboneJs,
             $underscoreJs,
+        ];
 
+        // Perhaps we show the widget on the dashboard.
+        $rollthedice = (float)rand() / (float)getrandmax();
+        if ($rollthedice < $this->eastereggchance) {
+            $widgetObj = new Widget();
+            $widgetObj
+                ->setZone('backend')
+                ->setLocation('dashboard_aside_middle')
+                ->setCallback([$this, 'backendDashboardWidget'])
+                ->setCallbackArguments([])
+                ->setDefer(false);
+            $assets[] = $widgetObj;
+        }
+
+        return $assets;
+    }
+
+
+    public function backendDashboardWidget()
+    {
+        $images = [
+            'https://media4.giphy.com/media/tIeCLkB8geYtW/giphy.gif',
+            'https://media.giphy.com/media/tITfss8cqzTO0/giphy.gif',
+            'https://media.giphy.com/media/jQqU9dCKUOdri/giphy.gif',
+            'https://media.giphy.com/media/UmayQNXJCo86I/giphy.gif',
+            'https://media.giphy.com/media/11ISwbgCxEzMyY/giphy.gif',
+            'https://media4.giphy.com/media/14bhmZtBNhVnIk/giphy.gif',
+            'https://media0.giphy.com/media/geYwtodB9AiI0/giphy.gif',
+            'https://media3.giphy.com/media/3o7abB06u9bNzA8lu8/giphy.gif'
+        ];
+        $image = $images[array_rand($images)];
+
+        // Render the template, and return the results
+        return $this->renderTemplate('widget_easteregg.twig', [
+            'version' => $this->version,
+            'image' => $image
+        ]);
+    }
+
+    public function registerServices(Application $app)
+    {
+        $seo = new SEO($this->getContainer(), $this->getConfig(), $this->version);
+        $app['twig']->addGlobal('seo', $seo);
+        $app['twig']->addGlobal('seoconfig', $this->getConfig());
+    }
+
+    protected function registerTwigPaths()
+    {
+        return [
+            'templates' => ['position' => 'prepend', 'namespace' => 'bolt']
         ];
     }
 

a91291b113aa98e21343bba1a27448cb7610d752
Bob den Otter <bobdenotter@gmail.com>
2016-11-22 18:28:40 +0100

    Update SeoExtension.php

--- src/SeoExtension.php
+++ src/SeoExtension.php
@@ -11,7 +11,7 @@ use Silex\Application;
 
 class SeoExtension extends SimpleExtension
 {
-    private $version = 'v0.11.0';
+    private $version = 'v0.12.0';
 
     public function registerFields()
     {
